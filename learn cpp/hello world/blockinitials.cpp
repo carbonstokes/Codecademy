20495b1f080e72caf57ced6ca31db00692d32d04
Charlie Stokes <charlie.g.stokes@gmail.com>
2020-11-12 13:07:04 +0000

    Create blockinitials.cpp

---
+++ learn cpp/hello world/blockinitials.cpp
@@ -0,0 +1,17 @@
+//I can surf!
+
+#include <iostream>
+
+int main() {
+
+std::cout << "  CCC        SSS\n";
+std::cout << "C    C      S   S\n";
+std::cout << "C           S    \n";
+std::cout << "C             SSS\n";
+std::cout << "C                S\n";
+std::cout << "C    C      S    S\n";
+std::cout << "  CCC        SSS\n";
+
+return 0;
+
+}
